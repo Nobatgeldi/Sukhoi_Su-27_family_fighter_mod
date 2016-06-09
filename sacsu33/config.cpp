ffa8bf95a8150fee39b4ee88a7fb2613856963f5
nobatgeldi <nobatgeldi.geldimammedow@outlook.com>
2016-06-10 02:50:32 +0300

    Added guns  and some texture

--- sacsu33/config.cpp
+++ sacsu33/config.cpp
@@ -4,145 +4,377 @@ class CfgPatches
 	{
 		units[]=
 		{
-			"SACSu33"
+			"SACSu33",
+			"SACSu33A"
 		};
 		weapons[]={};
 		requiredVersion=0.0.1;
 		requiredAddons[]={};
 	};
 };
-class CfgModels
+class CfgMagazines
 {
-	class Default
+	class VehicleMagazine;
+	class SAC_150Rnd_30mm_GSh301: VehicleMagazine
 	{
+		scope=2;
+		count=500;
+		ammo="B_35mm_AA";
+		initSpeed=1036;
+		tracersEvery=2;
+		nameSound="cannon";
 	};
-	class Name_Of_Your_Model : Default
+	class SAC_6Rnd_R73: VehicleMagazine
 	{
-
-		class Animations
+		scope=2;
+		displayName="R-73";
+		displayNameShort="R-73";
+		ammo="SAC_M_R73_AA";
+		count=6;
+		initSpeed=0;
+		maxLeadSpeed=450;
+		sound[]=
 		{
+			"A3\sounds_f\dummysound",
+			1,
+			1,
+			1300
+		};
+		reloadSound[]=
+		{
+			"A3\sounds_f\dummysound",
+			0.00031622799,
+			1,
+			20
+		};
+		nameSound="missiles";
+	};
+	class SAC_4Rnd_R73: SAC_6Rnd_R73
+	{
+		count=4;
+	};
+	class SAC_6Rnd_R27RE: SAC_6Rnd_R73
+	{
+		scope=2;
+		displayName="R-27RE";
+		count=6;
+		ammo="SAC_M_R27RE";
+		initSpeed=0;
+	};
+	class SAC_4Rnd_R27RE: SAC_6Rnd_R27RE
+	{
+		count=4;
+	};
+	class SAC_6Rnd_R77: VehicleMagazine
+	{
+		scope=2;
+		displayName="R-77";
+		count=6;
+		ammo="SAC_M_R77";
+		initSpeed=0;
+	};
+	class SAC_4Rnd_R77: SAC_6Rnd_R77
+	{
+		count=4;
+	};
+};
+class CfgAmmo
+{
+	class MissileBase;
+	class SAC_M_R73_AA: MissileBase
+	{
+		model="\SACSu33\SACR27RE.p3d";
+		proxyShape="\SACSu33\SACR27RE_proxy.p3d";
+		hit=200;
+		indirectHit=85;
+		indirectHitRange=10;
+		maneuvrability=27;
+		simulationStep=0.0020000001;
+		airLock=2;
+		irLock=1;
+		cost=1500;
+		maxSpeed=828;
+		timeToLive=40;
+		airFriction=0.050000001;
+		sideAirFriction=0.1;
+		trackOversteer=1;
+		trackLead=1;
+		initTime=0;
+		thrustTime=5;
+		thrust=240;
+		fuseDistance=500;
+		CraterEffects="AAMissileCrater";
+		explosionEffects="AAMissileExplosion";
+		//effectsMissile="FX_SU35_Missile_AA";
+		whistleDist=20;
+		muzzleEffect="BIS_fnc_effectFiredHeliRocket";
+		weaponLockSystem="2 + 16 + 4";
+		missileLockCone=120;
+		maxControlRange=8000;
+	};
+		class SAC_M_R27RE: SAC_M_R73_AA
+	{
+		model="\SACSu33\SACR27RE.p3d";
+		proxyShape="\SACSu33\SACR27RE_proxy.p3d";
+		hit=100;
+		indirectHit=50;
+		indirectHitRange=9;
+		maneuvrability=20;
+		simulationStep=0.0020000001;
+		trackOversteer=5;
+		trackLead=0.94999999;
+		airLock=1;
+		irLock=1;
+		initTime=0.30000001;
+		thrustTime=15;
+		thrust=400;
+		cost=10000;
+		maxSpeed=2300;
+		minRange=300;
+		minRangeProbab=0.1;
+		midRange=2000;
+		midRangeProbab=0.89999998;
+		maxRange=5000;
+		maxRangeProbab=0.69999999;
+		effectsMissile="SACmissile1";
+	};
+	class SAC_M_R77: SAC_M_R73_AA
+	{
+		model="\SACSu33\SACR77.p3d";
+		proxyShape="\SACSu33\SACR77_proxy.p3d";
+		hit=80;
+		indirectHit=40;
+		indirectHitRange=9;
+		maneuvrability=30;
+		simulationStep=0.0020000001;
+		trackOversteer=5;
+		trackLead=0.69999999;
+		airLock=1;
+		irLock=1;
+		thrustTime=12;
+		thrust=380;
+		cost=10000;
+		initTime=0.15000001;
+		maxSpeed=2100;
+		minRange=100;
+		minRangeProbab=0.1;
+		midRange=1800;
+		midRangeProbab=0.89999998;
+		maxRange=6000;
+		maxRangeProbab=0.69999999;
+		effectsMissile="SACmissile1";
+	};
+};
 
-			class nozzle1
+class CfgWeapons
+{
+	class CannonCore;
+	class SACGSh301: CannonCore
+	{
+        scope=2;
+		displayName="Gsh-301 30mm";
+		displayNameMagazine="Gsh-301 30mm";
+		shortNameMagazine="Gsh-301 30mm";
+		nameSound="cannon";
+		cursor="EmptyCursor";
+		cursorAim="mg";
+		magazines[]=
+		{
+			"SAC_150Rnd_30mm_GSh301"
+		};
+		canLock=2;
+		ballisticsComputer=2;
+		modes[]=
+		{
+			"manual",
+			"close",
+			"short",
+			"medium",
+			"far"
+		};
+		class GunParticles
+		{
+			class Effect
 			{
-				type="translation";
-				offset0=0.9;
-				offset1=-0.9;
-				axis="gear_l1_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle1";
-			};
-			class nozzle2
-			{
-				type="translation";
-				offset0=0.9;
-				offset1=-0.9;
-				axis="nozzle2_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle2";
-			};
-			class nozzle3
-			{
-				type="translation";
-				offset0=0.9;
-				offset1=-0.9;
-				axis="canopy_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle3";
-			};
-			class nozzle4
-			{
-				type="translation";
-				offset0=0.9;
-				offset1=-0.9;
-				axis="nozzle4_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle4";
-			};
-			class nozzle5
-			{
-				type="translation";
-				offset0=-0.9;
-				offset1=0.9;
-				axis="gear_l1_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle5";
-			};
-			class nozzle6
-			{
-				type="translation";
-				offset0=-0.9;
-				offset1=0.9;
-				axis="nozzle2_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle6";
-			};
-			class nozzle7
-			{
-				type="translation";
-				offset0=-0.9;
-				offset1=0.9;
-				axis="canopy_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle7";
-			};
-			class nozzle8
-			{
-				type="translation";
-				offset0=-0.9;
-				offset1=0.9;
-				axis="nozzle4_axis";
-				memory=1;
-				minValue=0;
-				maxValue=1;
-				minPhase=0;
-				maxPhase=1;
-				source="rpm";
-				sourceAddress=0;
-				selection="nozzle8";
+				effectName="MachineGun1";
+				positionName="nosegun";
+				directionName="nosegun_dir";
 			};
 		};
+		class manual: CannonCore
+		{
+			displayName="Gsh-301 30mm";
+			autoFire=1;
+			sounds[]=
+			{
+				"StandardSound"
+			};
+			class StandardSound
+			{
+				begin1[]=
+				{
+					"A3\Sounds_F\weapons\30mm\30mm_st_02",
+					1.99526,
+					1,
+					1500
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					1
+				};
+				weaponSoundEffect="DefaultRifle";
+			};
+			reloadTime=0.1;
+			dispersion=0.0024999999;
+			soundContinuous=0;
+			showToPlayer=1;
+			burst=1;
+			aiRateOfFire=0.5;
+			aiRateOfFireDistance=50;
+			minRange=1;
+			minRangeProbab=0.0099999998;
+			midRange=2;
+			midRangeProbab=0.0099999998;
+			maxRange=3;
+			maxRangeProbab=0.0099999998;
+			textureType="fullAuto";
+		};
+		class close: manual
+		{
+			showToPlayer=0;
+			burst=15;
+			aiRateOfFire=0.25;
+			aiRateOfFireDistance=400;
+			minRange=0;
+			minRangeProbab=0.050000001;
+			midRange=200;
+			midRangeProbab=0.69999999;
+			maxRange=400;
+			maxRangeProbab=0.2;
+		};
+		class short: close
+		{
+			burst=10;
+			aiRateOfFire=0.5;
+			aiRateOfFireDistance=500;
+			minRange=300;
+			minRangeProbab=0.2;
+			midRange=400;
+			midRangeProbab=0.69999999;
+			maxRange=500;
+			maxRangeProbab=0.2;
+		};
+		class medium: close
+		{
+			burst=7;
+			aiRateOfFire=1;
+			aiRateOfFireDistance=900;
+			minRange=400;
+			minRangeProbab=0.2;
+			midRange=700;
+			midRangeProbab=0.69999999;
+			maxRange=900;
+			maxRangeProbab=0.2;
+		};
+		class far: close
+		{
+			burst=4;
+			aiRateOfFire=1.5;
+			aiRateOfFireDistance=1500;
+			minRange=800;
+			minRangeProbab=0.2;
+			midRange=1000;
+			midRangeProbab=0.40000001;
+			maxRange=1500;
+			maxRangeProbab=0.0099999998;
+		};
+	};
+	class MissileBase;
+	class MissileLauncher;
+	class SACR73Launcher: MissileLauncher
+	{
+		canLock=2;
+		autoFire=0;
+		displayName="R-73 Archer";
+		displayNameMagazine="R-73 Archer";
+		shortNameMagazine="R-73";
+		cursorAim="EmptyCursor";
+		cursor="EmptyCursor";
+		minRange=300;
+		minRangeProbab=0.025;
+		midRange=2500;
+		midRangeProbab=0.090000004;
+		maxRange=9000;
+		maxRangeProbab=0.0099999998;
+		sounds[]=
+		{
+			"StandardSound"
+		};
+		class StandardSound
+		{
+			begin1[]=
+			{
+				"A3\Sounds_F\weapons\Rockets\missile_2",
+				1.12202,
+				1.3,
+				1000
+			};
+			soundBegin[]=
+			{
+				"begin1",
+				1
+			};
+			weaponSoundEffect="DefaultRifle";
+		};
+		soundFly[]=
+		{
+			"A3\Sounds_F\weapons\Rockets\rocket_fly_2",
+			1,
+			1.5,
+			700
+		};
+		lockingTargetSound[]=
+		{
+			"\A3\Sounds_F\weapons\Rockets\locked_1",
+			0.316228,
+			1
+		};
+		lockedTargetSound[]=
+		{
+			"\A3\Sounds_F\weapons\Rockets\locked_3",
+			0.316228,
+			2.5
+		};
+		reloadTime=0.1;
+		magazineReloadTime=0.1;
+		magazines[]=
+		{
+			"SAC_6Rnd_R73"
+		};
+		holdsterAnimValue=3;
+		weaponLockDelay=3;
+		textureType="semi";
+	};
+	class SACR27Launcher: SACR73Launcher
+	{
+		displayName="R-27RE";
+		magazines[]=
+		{
+			"SAC_6Rnd_R27RE",
+			"SAC_4Rnd_R27RE"
+		};
+		reloadTime=0.30000001;
+	};
+	class SACR77Launcher: SACR73Launcher
+	{
+		displayName="R-77";
+		magazines[]=
+		{
+			"SAC_6Rnd_R77",
+			"SAC_4Rnd_R77"
+		};
+		reloadTime=0.30000001;
 	};
 };
 
@@ -160,7 +392,7 @@ class CfgVehicles
 		DriverAction="pilot";
 		getInAction = GetInLow;
 		displayName="Su-33 Flanker";
-		model="\SACSu33\SACSu33B.p3d";
+		model="\SACSu33\SACSu33.p3d";
 		animated=1;
 		vehicleClass="Air";
 		icon="\SACSu33\paa\icon.paa";
@@ -191,8 +423,20 @@ class CfgVehicles
 		memoryPointGun="machinegun";
 		memoryPointLRocket="L raketa";
 		memoryPointRRocket="P raketa";
-		weapons[]={};
-		magazines[]={};
+		weapons[]=
+		{
+		 "SACGSh301",
+		 "SACR73Launcher",
+		 "SACR77Launcher",
+		 "SACR27Launcher"		 
+		};
+		magazines[]=
+		{
+		 "SAC_150Rnd_30mm_GSh301",
+         "SAC_4Rnd_R73",
+		 "SAC_4Rnd_R77",
+		 "SAC_4Rnd_R27RE"		 
+		};
 		aileronSensitivity=1.2;
 		elevatorSensitivity=1;
 		envelope[]={0,0.40000001,1.9,4,6.8000002,8.3000002,8.5,7.8000002,6.1999998,4.5999999,3.7,2.8,2.3,2,1.8,1.5,1.2,0.80000001,0.5,0.30000001,0.2,0};
@@ -424,4 +668,20 @@ class CfgVehicles
 			libTextDesc="The Sukhoi Su-33 (NATO reporting name Flanker-D)";
 		};
 	   };
+	class SACSu33B: SACSu33
+	{
+		displayName="Su-33 Flanker(FAB250,R-73,S-8T)";
+		model="\SACSu33\SACSu33B.p3d";
+		weapons[]=
+		{
+			"SACGSh301",
+			"SACR73Launcher",
+		};
+		magazines[]=
+		{
+			"SAC_150Rnd_30mm_GSh301",
+			"SAC_4Rnd_R73"
+		};
+		threat[]={1,1,0.85000002};
+	};
  };
